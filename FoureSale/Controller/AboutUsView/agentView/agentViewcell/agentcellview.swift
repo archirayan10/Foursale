f213ad14803b21e6eac5097f177a27b590775d60
sanjay <archirayan@archirayans-mac-mini.local>
2017-08-23 16:10:55 +0530

    sa

--- FoureSale/Controller/AboutUsView/agentView/agentViewcell/agentcellview.swift
+++ FoureSale/Controller/AboutUsView/agentView/agentViewcell/agentcellview.swift
@@ -5,6 +5,7 @@
 //  Created by archirayan on 8/23/17.
 //  Copyright © 2017 Author:  Archirayan infotech (dilip.bakotiya@gmail.com || info@archirayan.com ) Author URI: https://www.archirayan.com/. All rights reserved.
 // sanjay
+//jayes
 
 import UIKit
 
